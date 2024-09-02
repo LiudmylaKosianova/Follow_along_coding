affbeeb5c66188c46fbb665468fac2b177b6090f
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-02 16:23:26 +0200

    simple_client.cpp in progress

--- Network/src/simple_client.cpp
+++ Network/src/simple_client.cpp
@@ -11,6 +11,19 @@ enum class CustomMsgTypes : uint32_t
 int main()
 {
     olc::net::message<CustomMsgTypes> msg;
+    msg.header.id = CustomMsgTypes::FireBullet;
+
+    //testing
+    int a = 11;
+    bool b = true;
+    float c = 2.213f;
+
+    msg << a << b << c;
+
+    a = 99;
+    b = false;
+    c = 99.11f;
+ 
 
     return 0;
 
