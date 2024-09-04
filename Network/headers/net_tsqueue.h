68b58e6ace5fc58209d96a782f8f40a92ba200d3
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-04 09:31:39 +0200

    net_tsqueue.h finished

--- Network/headers/net_tsqueue.h
+++ Network/headers/net_tsqueue.h
@@ -12,6 +12,7 @@ namespace olc
         public:
             tsqueue() = default;
             tsuque(const tsqueue<T>&) = delete;
+            virtual ~tsqueue(){ clear();}
 
             //returns and maintains item at the front of Queue
             const T& front()
@@ -40,6 +41,46 @@ namespace olc
                 std::scoped_lock lock(muxQueue);
                 deqQueue.emplace_front(std::move(item));
             }
+
+            //return true if Queue has no items
+            bool empty()
+            {
+                std::scoped_lock lock(muxQueue);
+                return deqQueue.empty();
+            }
+
+            //return the number of items in Queue
+            size_t count()
+            {
+                std::scoped_lock lock(muxQueue);
+                return deqQueue.size();
+            }
+
+            //erase all items in Queue
+            void clear()
+            {
+                std::scoped_lock lock(muxQueue);
+                deqQueue.clear();
+            }
+
+            //romoves and returns item from the front of Queue
+            T pop_front()
+            {
+                std::scoped_lock lock(muxQueue);
+                auto t = std::move(deqQueue.front());
+                deqQueue.pop_front();
+                return t;
+            }
+
+            //romoves and returns item from the back of Queue
+            T pop_back()
+            {
+                std::scoped_lock lock(muxQueue);
+                auto t = std::move(deqQueue.back());
+                deqQueue.pop_back();
+                return t;
+            }
+
 
 
         protected:
