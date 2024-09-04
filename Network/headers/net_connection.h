386acf34a6520e65b2cdc1d50253b606f6dd2b8d
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-04 09:58:13 +0200

    net_connection.h finished

--- Network/headers/net_connection.h
+++ Network/headers/net_connection.h
@@ -25,7 +25,22 @@ namespace olc
         public:
             bool Send(const message<T>& msg);
 
-            
+        protected:
+            //each connection has a unique socket to a remote
+            asio::ip::tcp::socket m_socket;
+
+            //the context is shared with the whole asio instance
+            asio::io_context& m_asioContext;
+
+            //this queue holds all messages to be sent to the remote side of this connection
+            tsqueue<message<T>> m_qMessagesOut;
+
+            /*
+            this queue holds all messages that have been received from the romete side
+            of this connection. It is a reference, as the "owner" of this connection is
+            expected to provide a queue
+            */
+           tsqueue<owned_message>& m_qMessagesIn;
         };
     }
 }
